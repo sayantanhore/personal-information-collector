75d96989258c596d767264410eabf48996edcd49
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-10-31 19:19:59 +0200

    Filter list fixed

--- src/Components/FilterList/index.js
+++ src/Components/FilterList/index.js
@@ -14,35 +14,39 @@ export default class FilterList extends Component {
     };
     this.searchHandler = this.searchHandler.bind(this);
     this.selectItemOnEnter = this.selectItemOnEnter.bind(this);
+    this.chooseItem = this.chooseItem.bind(this);
   }
   shouldComponentUpdate(nextProps, nextState) {
+    if(nextState.lastChosenItem){
+      return true;
+    }
     if(this.state.highlighted === nextState.highlighted){
       return false;
     }
     return true;
   }
   componentDidUpdate() {
-    const items = $(ReactDOM.findDOMNode(this.refs.itemList)).find('li');
+    const items = $(this.refs.itemList).find('li');
     const selectedText = this.state.highlighted;
     if(selectedText === null) {
-      $(ReactDOM.findDOMNode(this.refs.searchListContainer)).scrollTop(0);
+      $(this.refs.searchListContainer).scrollTop(0);
     }
     else {
       let that = this;
       items.each(function(index) {
         if($(this).text() === selectedText) {
           let scrollTo = $(this);
-          let container = $(ReactDOM.findDOMNode(that.refs.searchListContainer));
+          let container = $(that.refs.searchListContainer);
           container.scrollTop(scrollTo.offset().top - container.offset().top + container.scrollTop());
         }
       });
     }
-    this.lastChosenItem = null;
+
   }
 
   searchHandler(event) {
-    const searchFor = $(ReactDOM.findDOMNode(event.target)).val();
-    const items = $(ReactDOM.findDOMNode(this.refs.itemList)).find('li');
+    const searchFor = $(event.target).val();
+    const items = $(this.refs.itemList).find('li');
     let that = this;
     let highlighted = null;
     items.each(function(index) {
@@ -51,23 +55,23 @@ export default class FilterList extends Component {
       }
     });
     this.setState({
-      highlighted: highlighted
+      highlighted: highlighted,
+      lastChosenItem: null
     });
   }
 
   selectItemOnEnter(event) {
     if(event.keyCode === 13) {
       if(this.state.highlighted !== null) {
-        /*
-        $(ReactDOM.findDOMNode(this.refs.selectedItemsContainer)).append(
-          '<div>' + this.state.highlighted + '<span>X</span></div>'
-        );
-        */
-        this.lastChosenItem = this.state.highlighted;
-        this.setState({highlighted: null});
+        this.setState({lastChosenItem: this.state.highlighted, highlighted: null});
       }
       $(event.target).val('');
     }
+  }
+
+  chooseItem(event) {
+    const searchFor = $(event.target).text();
+    this.setState({lastChosenItem: searchFor});
   }
 
   render() {
@@ -81,16 +85,16 @@ export default class FilterList extends Component {
             <ul ref="itemList">
               {this.props.items.map((item, index) => {
                 if(this.state.highlighted === item) {
-                  return <li key={index} className="highlight">{item}</li>
+                  return <li key={index} className="highlight" onClick={this.chooseItem}>{item}</li>
                 }
                 else {
-                  return <li key={index}>{item}</li>
+                  return <li key={index} onClick={this.chooseItem}>{item}</li>
                 }
               })}
             </ul>
           </div>
         </div>
-        <FilteredItems chosenItem={this.lastChosenItem}/>
+        <FilteredItems chosenItem={this.state.lastChosenItem}/>
       </div>
     );
   }
